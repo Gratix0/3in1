ae9d342ebd12f1d2f0e280d7fba97d8c331f9104
Gratix0 <oganisyan.sanya1@mail.ru>
2022-09-30 22:31:53 +0300

    First commit
    First commit

---
+++ 3 in 1/Program.cs
@@ -0,0 +1,142 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+int input;
+
+try
+{
+    do
+    {
+
+        Console.WriteLine("Choise a program\n");
+        System.Threading.Thread.Sleep(1500);
+
+        Console.WriteLine("Today's menu: \n");
+        Console.WriteLine("1. Game \"Guess the number\" ");
+        Console.WriteLine("2. Multiplication table:");
+        Console.WriteLine("3. Output of divisors of a number");
+        Console.WriteLine("0. if you want to leave... Traitor");
+
+
+        input = Convert.ToInt32(Console.ReadLine());
+
+        switch (input)
+        {
+            case 1:
+                Random rand = new Random();
+                int randomNum = rand.Next(101);
+                int count = 1;
+                Console.WriteLine("A number from 0 to 100 is guessed. Try to guess it in 15 attempts.");
+                Console.WriteLine("Enter the first number:");
+                
+                while (count <= 15)
+                {
+                    int msg = Convert.ToInt32(Console.ReadLine());
+                    if (count == 16)
+                    {
+                        Console.WriteLine("Alas, you did not get the number you had guessed. It was a number " + randomNum + "!");
+                        break;
+                    }
+
+                    if (msg > 100)
+                    {
+                        count++;
+                        Console.WriteLine("Загадано в диапозоне от 0 до 100! Но можешь продолжить))");
+                        continue;
+                    }
+
+                    if (msg < 0)
+                    {
+                        count++;
+                        Console.WriteLine("Загадано в диапозоне от 0 до 100! Но можешь продолжить))");
+                        continue;
+                    }
+
+                    if (randomNum == msg)
+                    {
+                        Console.WriteLine("Yes! The computer guessed the number " + msg + "!");
+                        break;
+                    }
+                    
+                    if (Math.Abs(randomNum - msg) < 10)
+                    {
+                        count++;
+                        Console.WriteLine("Почти угодал, горячевое");
+                        Console.WriteLine("Попытка № " + count + ":");
+                        continue;
+                    }
+                    if (Math.Abs(randomNum - msg) < 20)
+                    {
+                        count++;
+                        Console.WriteLine("Рядышком, тёпленьбкое");
+                        Console.WriteLine("Попытка № " + count + ":");
+                        continue;
+                    }
+                        if (Math.Abs(randomNum - msg) > 20)
+                    {
+                        count++;
+                        Console.WriteLine("Не угодал, холодненьбкое");
+                        Console.WriteLine("Попытка № " + count);
+                        continue;
+                    }
+                                        
+                    else
+                    {
+                        count++;
+                        Console.WriteLine("Играй по правилам)");
+                        
+                    }
+                }
+                break;
+
+            case 2:
+                {/* Мне больше нравиться такой вариант, ибо я нахожу его более лакончиным конкретно для этой задачи. 
+                 * Но как говориться спешел фо ю, сделаю и вариант с отработкой обработки двумерных массивов
+                 */
+                    Console.WriteLine("\ny/x |   1\t2\t3\t4\t5\t6\t7\t8\t9");
+                    Console.Write("--------------------------------------------------------------------------");
+                    Console.WriteLine();
+                    for (int ix = 1; ix < 10; ix++)
+                    {
+                        Console.Write(ix + "   |  ");
+                        for (int j = 1; j < 10; j++)
+                        {
+                            Console.Write("\t" + ix * j);
+                        }
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                    }
+                    Console.WriteLine("Tap any button to continue");
+                    Console.ReadLine();
+                    break;
+                }
+
+                /*
+                 
+                 */
+                
+
+            case 3:
+                {
+                    Console.WriteLine("Enter a number");
+                    
+                    for (int userMessage = Convert.ToInt32(Console.ReadLine()); userMessage % 2 == 0; userMessage++)
+                    { 
+                        
+                    }
+                    break;
+
+                }
+            case 0:
+                Console.WriteLine("\nk -. -");
+                break;
+            default:
+                Console.WriteLine("HAVE MERCY ON ME, DON'T BREAK CODE, JUST COISE NUM OF RANGE 0 TO 3");
+                break;
+        };
+    } while (input != 0);
+}
+catch
+{
+    Console.WriteLine("Not today :) Play by the rules next time");
+
+}
